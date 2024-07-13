39d9bfbbd891e637dda14c45d04972a263a08e1d
Dmitry Kovganov <dmitry.kovganov@gmail.com>
2024-07-13 17:09:17 +0300

    implemented User.Create method

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -4,11 +4,14 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math/rand"
 	"net"
+	"sync"
 
-	"github.com/brianvoe/gofakeit"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
 	desc "github.com/DmitryKovganov/go-microservices-auth/pkg/user_v1"
@@ -20,17 +23,80 @@ type server struct {
 	desc.UnimplementedUserV1Server
 }
 
-// Get ...
+type SyncMap struct {
+	users map[int64]*desc.User
+	m     sync.RWMutex
+}
+
+var state = &SyncMap{
+	users: make(map[int64]*desc.User),
+}
+
+func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("Create, req: %+v", req)
+
+	if req.GetName() == "" {
+		return nil, status.Error(codes.InvalidArgument, "Name is required")
+	}
+
+	if req.GetEmail() == "" {
+		return nil, status.Error(codes.InvalidArgument, "Email is required")
+	}
+
+	if req.GetPassword() == "" {
+		return nil, status.Error(codes.InvalidArgument, "Password is required")
+	}
+
+	if req.GetPasswordConfirm() == "" {
+		return nil, status.Error(codes.InvalidArgument, "PasswordConfirm is required")
+	}
+
+	if req.GetPassword() != req.GetPasswordConfirm() {
+		return nil, status.Error(codes.InvalidArgument, "PasswordConfirm and Password are not equal")
+	}
+
+	now := timestamppb.Now()
+	createdUser := &desc.User{
+		Id: rand.Int63(),
+		Info: &desc.UserInfo{
+			Name:     req.Name,
+			Email:    req.Email,
+			Password: req.Password,
+			Role:     req.Role,
+		},
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+
+	state.m.Lock()
+	defer state.m.Unlock()
+
+	state.users[createdUser.Id] = createdUser
+
+	return &desc.CreateResponse{
+		Id: createdUser.Id,
+	}, nil
+}
+
 func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
-	log.Printf("User id: %d", req.GetId())
+	log.Printf("Get, req: %+v", req.GetId())
+
+	if req.GetId() == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Id is required")
+	}
+
+	user, ok := state.users[req.GetId()]
+	if !ok {
+		return nil, status.Error(codes.InvalidArgument, "User not found")
+	}
 
 	return &desc.GetResponse{
-		Id:        req.GetId(),
-		Name:      gofakeit.BeerName(),
-		Email:     gofakeit.Email(),
-		Role:      desc.UserRole(gofakeit.Number(int(desc.UserRole_USER), int(desc.UserRole_ADMIN))),
-		CreatedAt: timestamppb.New(gofakeit.Date()),
-		UpdatedAt: timestamppb.New(gofakeit.Date()),
+		Id:        user.Id,
+		Name:      user.Info.Name,
+		Email:     user.Info.Email,
+		Role:      user.Info.Role,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
 	}, nil
 }
 
